791d4fbda22f0d7df11199fa964ed1cd7713e791
MuhammadYousuf20 <39589913+MuhammadYousufHere@users.noreply.github.com>
2018-10-04 00:19:59 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,66 @@
+$.ajax({
+    url:`https://newsapi.org/v2/top-headlines?sources=cnn&apiKey=8a228043b8ec42288a2b6aac8b6bbe63`,
+    success: function(data){
+       console.log(data)
+       for(i=0;i<4;i++){
+       document.querySelector(".world-news-area").innerHTML += `
+       <div class="card" >
+       <img class="card-img-top" src="${data.articles[i].urlToImage}" alt="Card image cap">
+       <div class="card-body">
+         <h5 class="card-title n1">${data.articles[i].title}</h5>
+         <p class="card-text nd1">${data.articles[i].description}</p>
+        <div class="read-more"><a href="${data.articles[i].url}" class="text-more">Read more</a></div>
+           </div>
+       </div>
+       `;
+    }
+       
+    }
+
+});
+
+$.ajax({
+    url:`https://newsapi.org/v2/top-headlines?sources=bbc-sport&apiKey=8a228043b8ec42288a2b6aac8b6bbe63`,
+    success: function(data){
+       console.log(data)
+       for(i=0;i<4;i++){
+       document.querySelector(".sport-news-area").innerHTML += `
+       <div class="card" >
+       <img class="card-img-top" src="${data.articles[i].urlToImage}" alt="Card image cap">
+       <div class="card-body">
+         <h5 class="card-title n1">${data.articles[i].title}</h5>
+         <p class="card-text nd1">${data.articles[i].description}</p>
+        <div class="read-more"><a href="${data.articles[i].url}" class="text-more">Read more</a></div>
+           </div>
+       </div>
+       `;
+    }
+       
+    }
+
+})
+
+$.ajax({
+    url:`https://newsapi.org/v2/top-headlines?sources=techradar&apiKey=8a228043b8ec42288a2b6aac8b6bbe63`,
+    success: function(data){
+       console.log(data)
+       for(i=0;i<4;i++){
+       document.querySelector(".tech-news-area").innerHTML += `
+       <div class="card" >
+       <img class="card-img-top" src="${data.articles[i].urlToImage}" alt="Card image cap">
+       <div class="card-body">
+         <h5 class="card-title n1">${data.articles[i].title}</h5>
+         <p class="card-text nd1">${data.articles[i].description}</p>
+        <div class="read-more"><a href="${data.articles[i].url}" class="text-more">Read more</a></div>
+           </div>
+       </div>
+       `;
+    }
+       
+    }
+
+})
+
+
+
+
